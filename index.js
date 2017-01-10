0e52ff19c016e6a2d7840728bbf1902b7f904e8d
lmk123 <me@limingkai.cn>
2017-01-11 00:37:40 +0800

    重构代码

--- index.js
+++ index.js
@@ -28,42 +28,25 @@ function convert2promise (v) {
  */
 function MultiLevel (query) {
   this._query = query
-  this._max = -1
   this.lists = []
   this.selected = []
-  this.ready = this._changed(-1)
+  this._max = -1
+  this.ready = this._loadList()
 }
 
 var p = MultiLevel.prototype = Object.create(TinyEmitter.prototype)
 p.constructor = MultiLevel
 
 /**
- * 当一个下拉框的值发生变化之后，使用这个方法会清空后续下拉框的选项和值，并重新请求下一个下拉框的选项
- * @param {number} index
- * @param {boolean} isClear - 如果设为 true，则说明此次修改是一次"清空"操作，不需要请求下一个下拉框的选项
+ * 加载下一个下拉框的列表
  * @return {Promise}
  * @private
  */
-p._changed = function (index, isClear) {
+p._loadList = function () {
+  var curIndex = this._max
   var selected = this.selected
-  var lists = this.lists
-  var curSelected = selected[index]
-  var next = index + 1
-
-  this._max = index
-
-  // 清空后面几个下拉框的选项和值
-  var deletedLists = lists.splice(next)
-  var deletedSelected = selected.splice(isClear ? index : next)
-  var length = deletedLists.length
-  if (length) {
-    this.emit('cut-off', length, this, deletedLists, deletedSelected)
-    this.emit('change', selected, this)
-  }
-
-  if (isClear && index !== -1) {
-    return resolvedPromise
-  }
+  var curSelected = selected[curIndex]
+  var next = curIndex + 1
 
   // 获取下一个列表的选项
   var _this = this
@@ -72,11 +55,11 @@ p._changed = function (index, isClear) {
     if (isEmptyArray(list)) return
 
     // 如果在查询期间，用户改变了其他下拉框或者重选了当前下拉框的值则不应用此次查询的结果
-    if (index !== _this._max || curSelected !== selected[index]) return
+    if (curIndex !== _this._max || curSelected !== selected[curIndex]) return
 
     // 新增一个列表
     _this._max = next
-    lists.push(list)
+    _this.lists.push(list)
     _this.emit('add', list, next, _this)
   })
 }
@@ -96,7 +79,9 @@ p.set = function (index, val, preventEvent) {
       return Promise.reject(new RangeError('Failed to set the ' + (index + 1) + 'th list\'s value: There are ' + (max + 1) + ' lists only.'))
     }
 
-    var list = _this.lists[index]
+    var lists = _this.lists
+    var list = lists[index]
+
     var notEmptyVal = val != null
 
     // 如果在列表中找不到用户要设置的值则 reject
@@ -104,11 +89,24 @@ p.set = function (index, val, preventEvent) {
       return Promise.reject(new Error('Failed to set the ' + (index + 1) + 'th list\'s value: Can\'t set a value that not exist in the ' + (index + 1) + 'th list.'))
     }
 
+    _this._max = index
+    var next = index + 1
     var selected = _this.selected
     if (notEmptyVal) selected[index] = val
     if (!preventEvent) _this.emit('set', val, index)
-    if (index === max && notEmptyVal) _this.emit('change', selected, _this)
-    return this._changed(index, !notEmptyVal)
+
+    // 清空后面几个下拉框的选项和值
+    var deletedLists = lists.splice(next)
+    var deletedSelected = selected.splice(notEmptyVal ? index + 1 : index)
+    var length = deletedLists.length
+    if (length) {
+      _this.emit('cut-off', length, _this, deletedLists, deletedSelected)
+    }
+    _this.emit('change', selected, _this)
+
+    if (notEmptyVal) {
+      return _this._loadList()
+    }
   })
 }
 
